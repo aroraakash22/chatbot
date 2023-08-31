7d4d3b5a34e8d58335c4e41d786f612276d547ca
aroraakash22 <143225075+aroraakash22@users.noreply.github.com>
2023-09-01 01:33:24 +0530

    chatbot_app.py

--- chatbot_app.py
+++ chatbot_app.py
@@ -1,66 +1,48 @@
 import openai
 import streamlit as st
 
-# Initialize OpenAI with the provided API key
+# Initialize OpenAI once an API key is entered
 def initialize_openai(api_key):
     openai.api_key = api_key
 
 # Fetch a response from GPT-3.5 Turbo model using the chat endpoint
-def get_openai_response(messages, directive=""):
+def get_openai_response(messages):
     try:
-        initial_message = {
-            "role": "system",
-            "content": directive
-        } if directive else {
-            "role": "user",
-            "content": "You are a helpful assistant."
-        }
-
         response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
-            messages=[initial_message, {"role": "user", "content": messages[-1]['content']}]
+            messages=messages
         )
         
         return response['choices'][0]['message']['content']
     except Exception as e:
         return str(e)
 
-st.title("PW Chatbots")
+with st.sidebar:
+    openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
+    st.markdown("[Get an OpenAI API key](https://platform.openai.com/account/api-keys)")
+    st.markdown("[View the source code](https://github.com/YourGitHub/YourRepoName)")
+    st.markdown("[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://YourGitHubCodespaceLink)")
 
-# Sidebar for settings
-st.sidebar.header("Settings")
-api_key = st.sidebar.text_input("Enter OpenAI API Key:", type="password")
-directive = st.sidebar.text_area("Enter Directive:")
+st.title("💬 PW Chatbot")
 
-# Initialize OpenAI if API key is present
-if api_key:
-    initialize_openai(api_key)
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [{"role": "system", "content": "You are a helpful assistant."}]
 
-# Message management
-messages = []
-
-# Capture user input
-user_input = st.text_input("You:")
-
-if user_input:
-    if api_key:  # Ensure API key is present
-        messages.append({"role": "user", "content": user_input})
-        bot_response = get_openai_response(messages, directive)
-        messages.append({"role": "assistant", "content": bot_response})
-        
-        # Clear the user input box
-        st.text_input("You:", value="", key="user_input")
-
-# Display chat history
-for message in messages:
-    role = message["role"].capitalize()
+for message in st.session_state.messages:
+    role = message["role"]
     content = message["content"]
-    st.write(f"{role}: {content}")
+    if role == "assistant":
+        st.container().write("PW Chatbot: " + content)
+    elif role == "user":
+        st.container().write("You: " + content)
 
-# Explanation in sidebar
-st.sidebar.markdown("""
-### How to use:
-1. Enter your OpenAI API key in the sidebar.
-2. (Optional) Provide a directive.
-3. Type your message and hit enter to chat.
-""")
+if openai_api_key:
+    initialize_openai(openai_api_key)
+    user_message = st.text_input("Enter your message:")
+    
+    if user_message:
+        st.session_state.messages.append({"role": "user", "content": user_message})
+        bot_response = get_openai_response(st.session_state.messages)
+        st.session_state.messages.append({"role": "assistant", "content": bot_response})
+else:
+    st.info("Please add your OpenAI API key in the sidebar.")
