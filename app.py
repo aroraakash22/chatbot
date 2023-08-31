c71107d787195705eb152a065247bf2c4365fc42
aroraakash22 <143225075+aroraakash22@users.noreply.github.com>
2023-09-01 01:55:38 +0530

    app.py

--- app.py
+++ app.py
@@ -1,36 +1,18 @@
-from pathlib import Path
-from random import randrange
 import time
 import openai
 import streamlit as st
-from project_directory.utils.styles.menu_styles import FOOTER_STYLES, HEADER_STYLES
-from utils.conversation import get_user_input, show_chat_buttons, show_conversation
-from utils.footer import show_donates, show_info
-from utils.helpers import get_files_in_dir, get_random_img
-from utils.lang import en, ru
-from streamlit_option_menu import option_menu
-
-# --- PATH SETTINGS ---
-current_dir: Path = Path(__file__).parent if "__file__" in locals() else Path.cwd()
-assets_dir: Path = current_dir / "assets"
-icons_dir: Path = assets_dir / "icons"
-img_dir: Path = assets_dir / "img"
-tg_svg: Path = icons_dir / "tg.svg"
 
 # --- GENERAL SETTINGS ---
 PAGE_TITLE: str = "AI Talks"
 PAGE_ICON: str = "🤖"
-LANG_EN: str = "En"
-LANG_RU: str = "Ru"
-AI_MODEL_OPTIONS: list[str] = ["gpt-3.5-turbo"]
-
 st.set_page_config(page_title=PAGE_TITLE, page_icon=PAGE_ICON)
 
-# Storing The Context
+# Set OpenAI API Key
+openai.api_key = st.sidebar.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
+
+# Initialize session state
 if "messages" not in st.session_state:
     st.session_state.messages = [{"role": "assistant", "content": "How can I help you?"}]
-
-openai.api_key = st.sidebar.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
 
 def typewriter_effect(message):
     for char in message:
