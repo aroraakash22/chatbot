17cef286d2d61a312747e33a1944d5fe38c2480d
aroraakash22 <143225075+aroraakash22@users.noreply.github.com>
2023-09-01 01:19:10 +0530

    chatbot_app.py

--- chatbot_app.py
+++ chatbot_app.py
@@ -1,46 +1,57 @@
-# chatbot_app.py
 import openai
 import streamlit as st
+import time
 
 # Initialize OpenAI once an API key is entered
 def initialize_openai(api_key):
     openai.api_key = api_key
 
-# Fetch response from GPT model
-def get_openai_response(prompt_text, directive):
+# Fetch response from GPT-3.5 Turbo model
+def get_openai_response(prompt_text, directive=""):
     prompt = directive + " " + prompt_text
-    response = openai.Completion.create(
-      engine="davinci",
-      prompt=prompt,
-      max_tokens=150
-    )
-    return response.choices[0].text.strip()
+    try:
+        response = openai.Completion.create(
+            engine="text-davinci-003",  # Use GPT-3.5 Turbo model
+            prompt=prompt,
+            max_tokens=150
+        )
+        return response.choices[0].text.strip()
+    except Exception as e:
+        return f"Error: {e}"
 
-st.title("OpenAI Chatbot")
+# Typewriter effect
+def typewriter(text):
+    for char in text:
+        st.write(char, end='', key='chat_output')
+        time.sleep(0.05)
 
-# API Key Input
-api_key = st.text_input("Enter your OpenAI API Key:", type="password")
+st.title("PW Chatbots")
+
+# Sidebar
+st.sidebar.header("Settings")
+api_key = st.sidebar.text_input("Enter OpenAI API Key:", type="password")
+directive = st.sidebar.text_area("Enter Directive:")
+
+# Initialize OpenAI if API key is present
 if api_key:
     initialize_openai(api_key)
 
-# Directive Input
-directive = st.text_area("Enter your custom directive:")
+# Chat UI
+user_input = st.text_input("You:")
 
-# User Input
-user_input = st.text_input("Ask the chatbot:")
+if user_input:
+    if api_key:  # Ensure API key is present
+        # Fetch chatbot response
+        chatbot_response = get_openai_response(user_input, directive)
+        # Display using typewriter effect
+        typewriter("Chatbot: " + chatbot_response)
+    else:
+        st.write("Please enter OpenAI API key in the sidebar.")
 
-if user_input and directive and api_key:
-    # Fetch the chatbot's answer
-    chatbot_response = get_openai_response(user_input, directive)
-    st.write("Chatbot:", chatbot_response)
-
-# Slider
-st.sidebar.slider('Set a custom value:', 0, 100, 50)
-
+# Explanation in sidebar
 st.sidebar.markdown("""
-### Instructions
-1. Enter your OpenAI API Key.
-2. Add a custom directive.
-3. Ask your question!
+### How to use:
+1. Enter your OpenAI API key in the sidebar.
+2. (Optional) Provide a directive.
+3. Type your message and hit enter to chat.
 """)
-
