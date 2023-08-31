0cbb1263188e094d69fbd7b578cf906987ba47f8
aroraakash22 <143225075+aroraakash22@users.noreply.github.com>
2023-09-01 02:24:51 +0530

    app.py

--- app.py
+++ app.py
@@ -2,39 +2,33 @@ import openai
 import streamlit as st
 import time
 
-# Set the icon and title of the chatbot
+# Set up the Streamlit app
 icon_url = "https://drive.google.com/uc?export=view&id=1qaSJMQRCQxCTlL-obX0T2YsP64rZq5sq"
 st.set_page_config(page_title="PW Chatbot", page_icon=icon_url)
 
-# Introduce a typewriter effect to the chat
-def typewriter_effect(text):
-    container = st.empty()
-    output_text = ""
-    for char in text:
+# Typewriter effect
+def typewriter_effect(content):
+    for char in content:
+        st.text(char, end='', key='typewriter_temp')
         time.sleep(0.05)
-        output_text += char
-        container.text(output_text)
 
-# Sidebar
+# Sidebar for API key and directives
 with st.sidebar:
     openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
-    directive = st.text_input("Directive for Chatbot")
+    directive = st.text_input("Directive for the chatbot:")
     st.markdown("[Get an OpenAI API key](https://platform.openai.com/account/api-keys)")
-    st.markdown("[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)")
-    st.markdown("[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)")
+    st.markdown("[View the source code on GitHub](https://github.com/your-repo-link-here)")
+    st.markdown("[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/repo-link-here?quickstart=1)")
 
 st.title("PW Chatbot")
-
 if "messages" not in st.session_state:
     st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
 
 for msg in st.session_state.messages:
-    role = msg["role"]
-    content = msg["content"]
-    if role == "assistant":
-        typewriter_effect(content)
+    if msg["role"] == "assistant":
+        typewriter_effect(msg["content"])
     else:
-        st.chat_message(role).write(content)
+        st.chat_message(msg["role"]).write(msg["content"])
 
 if prompt := st.chat_input():
     if not openai_api_key:
@@ -44,26 +38,10 @@ if prompt := st.chat_input():
     openai.api_key = openai_api_key
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
-    if directive:  
-        prompt = f"{directive}: {prompt}"
-    response = openai.Completion.create(model="gpt-3.5-turbo", messages=st.session_state.messages)  # fixed method name
-    msg = response.choices[0].message
-    st.session_state.messages.append(msg)
-    typewriter_effect(msg["content"])
-# ... [previous code]
 
-if prompt := st.chat_input():
-    if not openai_api_key:
-        st.warning("Please add your OpenAI API key to continue.")
-        st.stop()
-
-    openai.api_key = openai_api_key
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    st.chat_message("user").write(prompt)
-    
-    if directive:  
+    if directive:
         prompt = f"{directive}: {prompt}"
-    
+
     try:
         response = openai.Completion.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
         msg = response.choices[0].message
